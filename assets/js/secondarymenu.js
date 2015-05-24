8624ba479f8b1b31840b209600f0412f6d4d87b4
Luca Di Stefano <lucadistefano90@gmail.com>
2015-05-24 18:32:05 +0200

    messaggio <noscript>

--- assets/js/secondarymenu.js
+++ assets/js/secondarymenu.js
@@ -1,5 +1,5 @@
 $(function(){
-    var menu = $(".secondary-menu").hide();
+    var menu = $(".secondary-menu").css("text-align", "left").hide();
     $(window).load(function(){
         $(window).resize();
         menu.slideDown("fast");
@@ -9,7 +9,9 @@ $(function(){
     $(window).resize(function(){
         if($(window).width() > 767) {
             padding = $("#main_nav .active").position().left;
-            if(!element.hasClass("btn-group")) {
+            // Al form va aggiunto ulteriore padding per allinearlo alla voce
+            // di menu corrispondente
+            if(element.hasClass("form-inline")) {
                 padding += parseInt($("#main_nav .active a").css("padding-left"));
             }
             element.offset({"left": padding});
